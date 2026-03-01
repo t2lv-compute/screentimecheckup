757c2eba747d61501eb1e5b68780adea9a871f05
t2lv-compute <183874517+t2lv-compute@users.noreply.github.com>
2026-03-01 21:55:31 +0700

    whoopsie

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4036742830" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1208226716" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
